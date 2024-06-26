201b640f9e75e090bf1add3a6fcb289f1ff6ed7e
zhangshuaibao <zhangshuaibao@bixin.cn>
2024-06-26 17:27:46 +0800

    学习dubbo

--- CardoRPC/src/main/java/com/cardo/proxy/ProxyFactory.java
+++ CardoRPC/src/main/java/com/cardo/proxy/ProxyFactory.java
@@ -9,6 +9,7 @@ import com.cardo.register.MapRemoteRegister;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProxyFactory {
@@ -17,6 +18,14 @@ public class ProxyFactory {
         Object proxyInstance = Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[]{interfaceClass}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+                String mock = System.getProperty("mock");
+                if(mock!=null&&mock.startsWith("return:")){
+                    String result=mock.replace("return:","");
+                    return result;
+                }
+
+
                 Invocation invocation = new Invocation(interfaceClass.getName(),
                         method.getName(),
                         method.getParameterTypes(),
@@ -26,15 +35,24 @@ public class ProxyFactory {
                 //服务发现
                 List<URL> urls = MapRemoteRegister.get(interfaceClass.getName());
 
-                //负载均衡
-                URL url = LoadBalance.random(urls);
-                String result=null;
-                //服务调用
-                try{
-                    result = httpClient.send(url.getHostname(), url.getPort(), invocation);
 
-                }catch (Exception e){
-                    return "报错";
+                //服务调用
+                String result=null;
+                List<URL> invokedUrls = new ArrayList<>();
+                int max=3;
+                while (max>0){
+                    urls.remove(invokedUrls);
+                    URL url = LoadBalance.random(urls);
+                    invokedUrls.add(url);
+                    try{
+                        //负载均衡
+                        result = httpClient.send(url.getHostname(), url.getPort(), invocation);
+
+                    }catch (Exception e){
+                        if(max-- != 0) continue;
+                        //容错
+                        return "报错";
+                    }
                 }
 
                 return result;
